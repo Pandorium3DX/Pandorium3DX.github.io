7329ef16e42cbf0d03532b4e55f1d0a44aae9b49
Cam <cam@cammm.pw>
2021-01-12 19:33:49 +0000

    News color test

--- news.php
+++ news.php
@@ -1,50 +1,10 @@
 Pandorium 2.0|Welcome to Pandorium Release 2.0!
 We have a lot of things to show you with this update! <br>
 We've tried our hardest to include everything that the community has wanted, and we hope we have delivered! <br>
-+ New Poses
-+ New Hairs
-+ New Clothing Items
-+ Tails
-+ Ears
-+ Outfits
-+ Stability Improvements<br>|https://attachments.f95zone.to/2020/05/654387_Small.png||||
-Update v56|New update v56
-Man: <br>
-+ Eyebrow 1 SempiTerno <br>
-+ Tattoo 8 JHEKHO, Andruwu, Cherry <br>
-+ Facetatto 4 SempiTerno, CandyEviLZ, Cherry <br>
-Woman: <br>
-+ Eyebrown 1 CandyEviLZ<br>
-+ Tattoo 10 JHEKHO, SempiTerno, CandyEviLZ, Cherry, Ghug, SeaQueen <br>
-+ Facetatto 10 IvanaD, SempiTerno, CandyEviLZ, Cherry, SeaQueen <br>
-+ Stoking 18 IvanaD, CandyEviLZ <br>
-- Prints 69 IvanaD, CandyEviLZ, Cherry, SeaQueen <br>
-|https://board.meredian.io/uploads/monthly_2020_06/updatejuly.jpg|||||
-Meredian anniversary|Event, CLICK for more details!
-|https://board.meredian.io/uploads/monthly_2020_05/Meredian_birthday_client.jpg|https://board.meredian.io/topic/474-meredian-anniversary-event/||||
-Update v55|New update v55:
-+ New stockings by Sempiterno
-+ New hair for woman by Sempiterno
-+ New tattoos by Candyevilz, Seaqueen, lucifer and Marta
-+ New faceprints by Candyevilz
-+ New eyebrowns for man by andruwu
-+ New model of wings and added for males
-+ New prints by SeaQueen, Marta, Candyevilz and JulianoKali<br>|https://board.meredian.io/uploads/monthly_2020_05/3DXvers55.jpg||||
-Update .37|New update:
-+ New hair for woman
-+ 2 new hair for man
-+ 3 new pants for man
-+ New underwear for woman
-+ New accessories
-+ Rings for woman and man
-+ Fixed hair shader
-+ New prints and tattoos by SeaQueen<br>|https://board.meredian.io/uploads/monthly_2020_05/3DX39037.jpg||||
-Update|New update:
-+ 5 new hair for woman
-+ 2 new hair for man
-+ New topwear
-+ New stockings by Bionicle
-+ New face prints for woman by Bionicle
-+ New face prints for man by Bionicle
-+ New tattoos for woman by ManOf_Wisdom
-+ New prints by SeaQueen<br>|https://attachments.f95zone.to/2020/05/654387_Small.png||||
+<color=green>+ New Poses</color>
+<color=green>+ New Hairs</color>
+<color=green>+ New Clothing Items</color>
+<color=green>+ Tails</color>
+<color=green>+ Ears</color>
+<color=green>+ Outfits</color>
+<color=green>+ Stability Improvements</color><br>|https://attachments.f95zone.to/2020/05/654387_Small.png||||
