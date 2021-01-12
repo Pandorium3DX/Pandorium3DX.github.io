76a61dcbf6b212a6aa5f754f7b2ebb6d93bbaf61
Cam <cam@cammm.pw>
2021-01-12 19:40:11 +0000

    News article 1 posted.

--- news.php
+++ news.php
@@ -1,4 +1,4 @@
-Pandorium 2.0|Welcome to Pandorium Release 2.0!
+Pandorium 2.0|<color=purple>Welcome to Pandorium Release 2.0!</color>
 We have a lot of things to show you with this update! <br>
 We've tried our hardest to include everything that the community has wanted, and we hope we have delivered! <br>
 <color=green>+ New Poses</color>
@@ -7,4 +7,4 @@ We've tried our hardest to include everything that the community has wanted, and
 <color=green>+ Tails</color>
 <color=green>+ Ears</color>
 <color=green>+ Outfits</color>
-<color=green>+ Stability Improvements</color><br>|https://attachments.f95zone.to/2020/05/654387_Small.png||||
+<color=green>+ Stability Improvements</color><br>|https://cdn.pandorium.net/Release/placeholder.png||||
