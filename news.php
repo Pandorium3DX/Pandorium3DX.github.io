fb454c4bb97b29ef00f0d5687aad5815cee31f6a
Cam <cam@cammm.pw>
2021-01-12 19:24:47 +0000

    whee

--- news.php
+++ news.php
@@ -1,3 +1,43 @@
+Reset|On October 11, we are cleaning all coins and photo slots from users. On our server for a long time, coins have no value associated with various factors. The cleanup will affect everyone. The new economy will be more thoughtful, the number of coins received will be reduced, but new ways of obtaining coins, more interesting ways are planned. More details will come later|||
+Update|
+|https://board.meredian.io/uploads/monthly_2020_09/meredian-v57.png.8dd105c64756b30014f4941e60a876c1.png|||
+2D and 3D Desginer|We work look for 2D and 3D Designer. Discord SempiTerno [MDC]SempiTerno#7496|https://board.meredian.io/uploads/monthly_2020_09/1MEREDIAN-2-D-AND-3-D.jpg.ff5050f7da42f3fb87b05fd9786e4b65.jpg|http://DISCORD.GG/uQPEazf||
+Update|
+|https://board.meredian.io/uploads/monthly_2020_09/meredian-v57.png.8dd105c64756b30014f4941e60a876c1.png|||
+Gratitude|
+We receive many proposals for donations, it is valuable to us. We open such an opportunity for everyone. In addition, in gratitude you will receive thanks, colored nicknames. All details on site.
+|https://board.meredian.io/uploads/monthly_2020_08/Gr3.png.b4958df30cc115af6d0753582a434972.png|https://board.meredian.io/topic/568-donation/||||
+Update v56|New update v56
+Man: <br>
++ Eyebrow 1 SempiTerno <br>
++ Tattoo 8 JHEKHO, Andruwu, Cherry <br>
++ Facetatto 4 SempiTerno, CandyEviLZ, Cherry <br>
+Woman: <br>
++ Eyebrown 1 CandyEviLZ<br>
++ Tattoo 10 JHEKHO, SempiTerno, CandyEviLZ, Cherry, Ghug, SeaQueen <br>
++ Facetatto 10 IvanaD, SempiTerno, CandyEviLZ, Cherry, SeaQueen <br>
++ Stoking 18 IvanaD, CandyEviLZ <br>
+- Prints 69 IvanaD, CandyEviLZ, Cherry, SeaQueen <br>
+|https://board.meredian.io/uploads/monthly_2020_06/updatejuly.jpg|||||
+Meredian anniversary|Event, CLICK for more details!
+|https://board.meredian.io/uploads/monthly_2020_05/Meredian_birthday_client.jpg|https://board.meredian.io/topic/474-meredian-anniversary-event/||||
+Update v55|New update v55:
++ New stockings by Sempiterno
++ New hair for woman by Sempiterno
++ New tattoos by Candyevilz, Seaqueen, lucifer and Marta
++ New faceprints by Candyevilz
++ New eyebrowns for man by andruwu
++ New model of wings and added for males
++ New prints by SeaQueen, Marta, Candyevilz and JulianoKali<br>|https://board.meredian.io/uploads/monthly_2020_05/3DXvers55.jpg||||
+Update .37|New update:
++ New hair for woman
++ 2 new hair for man
++ 3 new pants for man
++ New underwear for woman
++ New accessories
++ Rings for woman and man
++ Fixed hair shader
++ New prints and tattoos by SeaQueen<br>|https://board.meredian.io/uploads/monthly_2020_05/3DX39037.jpg||||
 Update|New update:
 + 5 new hair for woman
 + 2 new hair for man
