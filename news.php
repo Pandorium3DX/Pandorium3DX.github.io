86fea7bbffd2bd897ee535a15984dc3c9b6d0d84
Cam <cam@cammm.pw>
2021-01-26 11:29:04 +0000

    hm
    hm

--- news.php
+++ news.php
@@ -7,7 +7,7 @@ We've tried our hardest to include everything that the community has wanted, and
 <color=green>+ Tails</color>
 <color=green>+ Ears</color>
 <color=green>+ Outfits</color>
-<color=green>+ Stability Improvements</color><br>|https://cdn.discordapp.com/attachments/791345282578513930/803585887484510259/meredian-v57.png||||
+<color=green>+ Stability Improvements</color><br>|https://cdn.discordapp.com/attachments/791345282578513930/803586970244743218/poster429.jpg||||
 Pandorium 2.0|<color=purple>Welcome to Pandorium Release 2.0!</color>
 We have a lot of things to show you with this update! <br>
 We've tried our hardest to include everything that the community has wanted, and we hope we have delivered! <br>
