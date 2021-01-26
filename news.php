6e8840afe82b7d5eae90d7715e1f273a4af34ad7
Cam <cam@cammm.pw>
2021-01-26 13:56:04 +0000

    Test 3
    Test all 3

--- news.php
+++ news.php
@@ -17,4 +17,14 @@ We've tried our hardest to include everything that the community has wanted, and
 <color=green>+ Tails</color>
 <color=green>+ Ears</color>
 <color=green>+ Outfits</color>
-<color=green>+ Stability Improvements</color><br>|https://cdn.pandorium.net/Release/placeholder_news_small.png
+<color=green>+ Stability Improvements</color><br>|https://cdn.pandorium.net/Release/placeholder_news_small.png||||
+Pandorium 2.0|<color=purple>Welcome to Pandorium Release 2.0!</color>
+We have a lot of things to show you with this update! <br>
+We've tried our hardest to include everything that the community has wanted, and we hope we have delivered! <br>
+<color=green>+ New Poses</color>
+<color=green>+ New Hairs</color>
+<color=green>+ New Clothing Items</color>
+<color=green>+ Tails</color>
+<color=green>+ Ears</color>
+<color=green>+ Outfits</color>
+<color=green>+ Stability Improvements</color><br>|https://cdn.discordapp.com/attachments/791345282578513930/803599369935912980/poster413.png
