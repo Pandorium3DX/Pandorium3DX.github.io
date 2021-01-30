bae3cdab2f28be584ba8890971539643db4a4f86
Cam <Contillex@gmail.com>
2021-01-30 14:31:58 +0000

    Update news.php

--- news.php
+++ news.php
@@ -1,30 +1,11 @@
-Pandorium 2.0|<color=purple>Welcome to Pandorium Release 2.0!</color>
+Pandorium 2.0 BETA|<color=purple>Welcome to Pandorium Release 2.0 BETA PREVIEW!</color>
 We have a lot of things to show you with this update! <br>
 We've tried our hardest to include everything that the community has wanted, and we hope we have delivered! <br>
-<color=green>+ New Poses</color>
+<color=green>+ New Poses (Coming Soon)</color>
 <color=green>+ New Hairs</color>
 <color=green>+ New Clothing Items</color>
 <color=green>+ Tails</color>
 <color=green>+ Ears</color>
 <color=green>+ Outfits</color>
+<color=green>+ New Sky Shader</color>
 <color=green>+ Stability Improvements</color><br>|https://cdn.pandorium.net/Release/placeholder_news_small.png||||
-Pandorium 2.0|<color=purple>Welcome to Pandorium Release 2.0!</color>
-We have a lot of things to show you with this update! <br>
-We've tried our hardest to include everything that the community has wanted, and we hope we have delivered! <br>
-<color=green>+ New Poses</color>
-<color=green>+ New Hairs</color>
-<color=green>+ New Clothing Items</color>
-<color=green>+ Tails</color>
-<color=green>+ Ears</color>
-<color=green>+ Outfits</color>
-<color=green>+ Stability Improvements</color><br>|https://cdn.pandorium.net/Release/placeholder_news_small.png||||
-Pandorium 2.0|<color=purple>Welcome to Pandorium Release 2.0!</color>
-We have a lot of things to show you with this update! <br>
-We've tried our hardest to include everything that the community has wanted, and we hope we have delivered! <br>
-<color=green>+ New Poses</color>
-<color=green>+ New Hairs</color>
-<color=green>+ New Clothing Items</color>
-<color=green>+ Tails</color>
-<color=green>+ Ears</color>
-<color=green>+ Outfits</color>
-<color=green>+ Stability Improvements</color><br>|https://cdn.pandorium.net/Release/placeholder_news_small.png
