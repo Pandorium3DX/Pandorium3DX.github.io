b3fbf08769c907e53db2ac523f8d089d5db44eca
Cam <Contillex@gmail.com>
2021-02-12 21:35:39 +0000

    Update news.php

--- news.php
+++ news.php
@@ -1,3 +1,7 @@
+
+Intima.|First of all, the entire staff would like to welcome you all to the launch of Intima! This is a new virtual adult 3d world where you can explore, create, and live your fantasies! In light of recent events, we would like to all make a few statements. First of all, we strive to build a respectful community, where the quality of our members and the game is most important. As staff, our main goal is to support you, the community, in the most respectful way possible. Open and honest communication is not only what we will give you, but what we expect from everyone. Now go forth, bring light into the world, enjoy Intima!
+<br>
+<i>Click the image below to join our discord community!</i>|https://cdn.discordapp.com/attachments/808766552488804393/809886585100959764/placeholder_news_small.png|https://discord.com/invite/zx9ZByTdBM||
 Pandorium 2.0 BETA|<color=purple>Welcome to Pandorium Release 2.0 BETA PREVIEW!</color>
 We have a lot of things to show you with this update! <br>
 We've tried our hardest to include everything that the community has wanted, and we hope we have delivered! <br>
